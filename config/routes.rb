e68dec5bd029c0f027db3f04fea00ef9befe69c5
cedric cazal <cedcaz@gmail.com>
2022-08-19 11:11:14 -0500

    creation controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
